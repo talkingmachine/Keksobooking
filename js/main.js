d6007880b9344ffcc8badb5cbc6b8344d57c1674
talkingmachine <aeyueoq@gmail.com>
2022-06-08 01:07:55 +0300

    Main.js updated - added random number generate functions

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const getRandomInt = function (min, max) {
+  if (max < min) {
+    throw 'getRandomInt Error: max value less then min value';
+  }
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+const getRandomNumber = function (min, max, fractionDigits) {
+  if (max < min) {
+    throw 'getRandomNumber Error: max value less then min value';
+  }
+  let randomNumber = Math.random() * (max - min) + min
+  return randomNumber.toFixed(fractionDigits);
+}
+
+
+console.log(getRandomInt(0.1, 15)) //DELETE
+console.log(getRandomNumber(1, 12, 3)) //DELETE
+
